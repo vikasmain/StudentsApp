4dd973e312f8d3525e9d5462e8d95076c86de616
vikasmain <vikpichai@gmail.com>
2024-01-30 09:04:30 +0530

    Add bottom navigation bar in main activity

---
+++ app/src/main/java/com/example/studentsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.studentsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.studentsapp.ui.theme.StudentsAppTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    true
+                }
+
+                R.id.quiz -> {
+                    true
+                }
+
+                R.id.profile -> {
+                    true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+}
