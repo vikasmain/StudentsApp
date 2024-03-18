550e83d394d142278849addcbe2df09f0bcf0851
vikasmain <hellovikas12345@gmail.com>
2024-03-18 21:18:57 +0530

    Fixed recycler view items content

--- app/src/main/java/com/example/studentsapp/view/adapters/FeedPagingAdapter.kt
+++ app/src/main/java/com/example/studentsapp/view/adapters/FeedPagingAdapter.kt
@@ -3,6 +3,7 @@ package com.example.studentsapp.view.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +13,11 @@ import com.example.studentsapp.model.FeedResponse
 class FeedPagingAdapter :
     PagingDataAdapter<FeedResponse.FeedItem, FeedPagingAdapter.FeedViewHolder>(COMPARATOR) {
 
-    class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class FeedViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindDetails(item: FeedResponse.FeedItem) {
-
+            itemView.findViewById<TextView>(R.id.title).text = item.title
+            itemView.findViewById<TextView>(R.id.description).text = item.description
         }
     }
 
